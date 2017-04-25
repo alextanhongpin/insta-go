2880da3c3b9e57fe914a27b6d03e0204ba8db486
Alex Tan Hong Pin <alextan220990@gmail.com>
2017-04-25 18:08:00 +0800

    Update model.go

--- likesvc/model.go
+++ likesvc/model.go
@@ -4,8 +4,8 @@ import "time"
 
 // Like is the model created to store the user/photo pair
 type Like struct {
-	UserID      string    `json:"user_id"`
-	PhotoID     string    `json:"photo_id"`
-	DateCreated time.Time `json:"date_created"`
-	DateUpdated time.Time `json:"date_updated"`
+	UserID      string    `json:"user_id,omitempty"`
+	PhotoID     string    `json:"photo_id,omitempty"`
+	DateCreated time.Time `json:"date_created,omitempty"`
+	DateUpdated time.Time `json:"date_updated,omitempty"`
 }
