e091fa64b57599132c627210c43eee935cc4cc4c
Alex Tan Hong Pin <alextan220990@gmail.com>
2017-04-25 17:56:46 +0800

    Update service.go

--- likesvc/service.go
+++ likesvc/service.go
@@ -1,3 +1,4 @@
+// likesvc/service.go contains the methods to carry out CRUD operations for the `likes` resources 
 package likesvc
 
 import (
@@ -6,27 +7,27 @@ import (
 	_ "github.com/lib/pq"
 )
 
-// Service holds the database context
+// Service holds the db context
 type Service struct {
 	*sql.DB
 }
 
-// Register checks if the user can register a new account
-func (s Service) Like(req Like) (bool, error) {
-	_ = s.QueryRow("INSERT INTO likes (user_id, photo_id) VALUES ($1, $2)", req.UserID, req.PhotoID)
-	return true, nil
+// Like creates a new entry
+func (s Service) Like(req Like) error {
+	_, err := s.Exec("INSERT INTO likes (user_id, photo_id) VALUES ($1, $2)", req.UserID, req.PhotoID)
+	return err
 }
 
-func (s Service) Unlike(req Like) (bool, error) {
-	_ = s.QueryRow("DELETE FROM likes WHERE user_id = $1 AND photo_id = $2", req.UserID, req.PhotoID)
-	return true, nil
+// Unlike remove the entry
+func (s Service) Unlike(req Like) error {
+	_, err := s.Exec("DELETE FROM likes WHERE user_id = $1 AND photo_id = $2", req.UserID, req.PhotoID)
+	return err
 }
 
+// Count returns the like count for a photo
 func (s Service) Count(req Like) (int64, error) {
 	var count sql.NullInt64
-	err := s.QueryRow("SELECT count(photo_id) FROM likes WHERE photo_id = $1", req.PhotoID).Scan(&count)
-
-	if err != nil {
+	if err := s.QueryRow("SELECT count(photo_id) FROM likes WHERE photo_id = $1", req.PhotoID).Scan(&count); err != nil {
 		return 0, err
 	}
 
@@ -35,6 +36,3 @@ func (s Service) Count(req Like) (int64, error) {
 	}
 	return 0, nil
 }
-
-// Get the count of photos for each users group by user id
-// select count(user_id), photos.user_id from photos group by user_id;
